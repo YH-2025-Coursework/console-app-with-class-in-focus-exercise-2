95a6f2062933d25b30680971e47b86663cad8228
Lana <virsen.sv@gmail.com>
2025-10-04 19:45:12 +0200

    Create class Motorcycle

---
+++ Motorcycle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_app_with_Class_in_focus_Exercise_2
+{
+    public class Motorcycle : Vehicle
+    {
+        public override void CalculateEfficiency()
+        {
+            Console.WriteLine($"The motorcycle {Name} would come {100 / FuelConsumption} km on 10 liters of fuel.");
+        }
+
+    }
+}
